535bbb204966f27fb56377c3e98e1b5c263e3514
Shifatur Rahman <284788249+shifaturrahman10@users.noreply.github.com>
2026-05-21 18:04:11 +0600

    Refactor theme toggle function and improve scrolling effect

--- script.js
+++ script.js
@@ -30,14 +30,44 @@ function type() {
 type();
 
 // DARK MODE TOGGLE (FIXED)
-function toggleTheme() {
+function toggleMode(){
+
   document.body.classList.toggle("dark");
 
   let btn = document.getElementById("themeBtn");
 
-  if (document.body.classList.contains("dark")) {
+  if(document.body.classList.contains("dark")){
     btn.innerHTML = "☀️";
-  } else {
+  }else{
     btn.innerHTML = "🌙";
   }
+
 }
+const sections = document.querySelectorAll("h2[id]");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll",()=>{
+
+  let current = "";
+
+  sections.forEach(section=>{
+
+    const sectionTop = section.offsetTop;
+
+    if(pageYOffset >= sectionTop - 300){
+      current = section.getAttribute("id");
+    }
+
+  });
+
+  navLinks.forEach(link=>{
+
+    link.classList.remove("active");
+
+    if(link.getAttribute("href") === "#" + current){
+      link.classList.add("active");
+    }
+
+  });
+
+});
